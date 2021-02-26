9efa95286b6bbe5623214dbc0f7b0187f1c79e03
DDdestroyer10 <72734861+DDdestroyer10@users.noreply.github.com>
2021-02-26 15:04:39 +0530

    Add files via upload

--- Bird.js
+++ Bird.js
@@ -1,33 +1,32 @@
 class Bird {
 
-    constructor(x,y,r,angle){
-   var options={
-       isStatic:false,
-       restitution:0.8,
-       friction:0.5,
-       density:1.2
-   }
-    this.x=x;
-    this.y=y;
-    this.r=r;
-    this.body=Bodies.circle(this.x,this.y,this.r/2,options)
-    World.add(world,this.body)
-    
-    }
-    display(){
-      var angle = this.body.angle;
-      var paperpos = this.body.position; 
-      push()
-      fill(225)
-      strokeWeight(2);
-      translate(paperpos.x,paperpos.y);
-      rotate(angle)
-      ellipseMode(CENTER)
-      ellipse(paperpos.x,paperpos.y,this.r,this.r)
-      pop()
- 
-    }
- 
- 
- 
+  constructor(x,y,r){
+ var options={
+     isStatic:false,
+     restitution:1,
+     friction:0.5,
+     density:1.3
  }
+  this.x=x;
+  this.y=y;
+  this.r=r;
+  this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+  World.add(world,this.body)
+  
+  }
+  display(){
+    var paperpos = this.body.position;
+
+    push()
+    translate(paperpos.x,paperpos.y);
+    ellipseMode(CENTER)
+    ellipse( 0,0,this.r,this.r)
+    strokeWeight(2);
+    fill(255,0,255)
+    pop()
+
+  }
+
+
+
+}
--- sketch.js
+++ sketch.js
@@ -7,23 +7,27 @@ var engine, world;
 var w1,w2,w3,w4;
 var start = 0;
 var play = 1;
-var bird;
+var bird,b2;
+var c,c1,c2,c3,c4,c5,c6,c7;
 
 function setup() {
   createCanvas(800, 800);
   engine = Engine.create();
   world = engine.world;
+
   w1 = new Wall(5,400,20,800)
   w2 = new Wall(400,5,800,20)
   w3 = new Wall(795,400,20,800)
   w4 = new Wall(400,795,800,20)
-  bird = new Bird(40,40,50)
+  bird = new Bird(400,40,50)
+  c = new Wall(10)
+
 }
  
 function draw() {
 background("black")
-Engine.update(engine);
 
+Engine.update(engine);
 
 w1.display()
 w2.display()
@@ -31,6 +35,19 @@ w3.display()
 w4.display()
 bird.display()
 
-
   drawSprites()
+}
+
+
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW) {
+
+  Matter.Body.applyForce(bird.body,bird.body.position,{x:40,y:-100});
+
+  }
+  if (keyCode === LEFT_ARROW) {
+
+    Matter.Body.applyForce(bird.body,bird.body.position,{x:-40,y:-100});
+  
+    }
 }
